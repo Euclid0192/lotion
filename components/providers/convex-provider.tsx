0a047096fad55a2bc038e39a65b284355db55d53
Nam Nguyen <nguy1053@msu.edu>
2026-02-16 16:15:28 -0500

    Convex and Clerk setup

---
+++ components/providers/convex-provider.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { ReactNode } from "react";
+import { ConvexReactClient } from "convex/react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ClerkProvider, useAuth } from "@clerk/clerk-react";
+
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+    throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file');
+}
+
+const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
+
+// TODO: Refractor this later with a generic environment vars getter
+if (!process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY) {
+    throw new Error('Missing NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env file');
+}
+
+export const ConvexClientProvider = ({ children }: { children: ReactNode }) => {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY || ''}
+    >
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+};
